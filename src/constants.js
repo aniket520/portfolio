5b299818040de3ab27a05560b72afcb8ba0e7d08
MyName <aniketgadekar470@gmail.com>
2026-02-19 17:43:12 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,299 @@
+// Skills Section Logo's
+import htmlLogo from './assets/Skills/html-CfI0Cw-d.png';
+import cssLogo from './assets/Skills/css-BDv6rEn7.png';
+import sassLogo from './assets/Skills/sass-DcIBNIKX.png';
+import javascriptLogo from './assets/Skills/javascript-Bh7ioiOO.png';
+import reactjsLogo from './assets/Skills/reactjs-LYxsbuPE.png';
+import angularLogo from './assets/Skills/agc_logo-P2zJ24I3.png';
+import reduxLogo from './assets/Skills/redux-Cr5aXX9_.png';
+import nextjsLogo from './assets/Skills/nextjs-RkI80A9j.png';
+import tailwindcssLogo from './assets/Skills/tailwindcss-CCkYV0vR.png';
+import gsapLogo from './assets/Skills/gsap-B0fc-zKj.png';
+import materialuiLogo from './assets/Skills/materialui-COLpx5Sm.png';
+import bootstrapLogo from './assets/Skills/bootstrap-DnBAHoRv.png';
+import springbootLogo from './assets/Skills/springboot-DFKzz4Ke.png';
+import nodejsLogo from './assets/Skills/nodejs-BctxLIqp.png';
+import expressjsLogo from './assets/Skills/express-CnNYXM05.png';
+import mysqlLogo from './assets/Skills/mysql-WUjZbfAJ.png';
+import mongodbLogo from './assets/Skills/mongodb-Dz5Ie_AU.png';
+import firebaseLogo from './assets/Skills/firebase-CIJ6lDRj.png';
+import cLogo from './assets/Skills/c-CwQ4eILz.png';
+import cppLogo from './assets/Skills/cpp-DiyJqP-z.png';
+import javaLogo from './assets/Skills/java-Cr8sluZA.png';
+import pythonLogo from './assets/Skills/python-BIwUd1eo.png';
+import typescriptLogo from './assets/Skills/typescript-pf-J0Iah.png';
+import gitLogo from './assets/Skills/git-BClPRNCD.png';
+import githubLogo from './assets/Skills/github-DabB8wTb.png';
+import vscodeLogo from './assets/Skills/vscode-DI9n3a0r.png';
+import postmanLogo from './assets/Skills/postman-DSxPNzLU.png';
+import mcLogo from './assets/Skills/mc-DQLdLEV_.png';
+import figmaLogo from './assets/Skills/figma-CGxV7TkU.png';
+import netlifyLogo from './assets/Skills/netlify-CdgZKT40.png';
+import vercelLogo from './assets/Skills/vercel-B3cpfMqI.png';
+import postgreLogo from './assets/Skills/postgre-D8JQljwx.png';
+import csharpLogo from './assets/Skills/csharp-Dv0bCWsP.png';
+
+// Experience Section Logo's
+import snMastermind from './assets/work/SN_Mastermind.jpeg';
+import TechnoGrowth from './assets/work/Technogrowth.jpg';
+import Education4ol from './assets/work/Education4ol.jpeg';
+
+// Education Section Logo's
+import smt from './assets/Education/smt.jpeg';
+import newEng from './assets/Education/newEng.jpeg';
+import Engineering from './assets/Education/engineeringedu.jpeg';
+
+// Project Section Logo's
+import multivendorEcom from './assets/Projects/multivendor_ecom.jpg';
+import BloggingApp from './assets/Projects/BloggingApp.jpg';
+// import ExpenseManager from 'src\assets\Projects\Expense_Manager.jpg';
+import WebBrowser from './assets/Projects/WebBrowser.jpg';
+// import npmLogo from './assets/work_logo/npm.png';
+// import webverLogo from './assets/work_logo/web_dig.png';
+// import cmLogo from './assets/work_logo/cm.png';
+// import imagesearchLogo from './assets/work_logo/image_search.png';
+// import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: snMastermind,
+      role: "Junior Software Developer",
+      company: "SN Mastermind Pvt.lmt",
+      date: "Jan 2025 - Jan 2026",
+      desc: " Worked with a team of Java developers to develop enterprise applications for the banking and dairy domain. Learned under senior guidance and contributed to building backend modules using Spring Boot. Implemented REST APIs, handled database operations, and improved application performance and data management. Gained practical experience in real-world business logic and production-level development.",
+      skills: [
+        "HTML",
+        "CSS3",
+        "JavaScript",
+        "React JS",
+        "Java",
+        "Tailwind CSS",
+        "MySQL",
+        "Hibernate",
+        "SpringBoot",
+        "RestApi",
+        "Git",
+        "Postman"
+        
+      ],
+    },
+    {
+      id: 1,
+      img: TechnoGrowth,
+      role: "Fullstack Developer Intern",
+      company: "Technogrowth Solutions",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to enterprise web applications as a Java Full-Stack Developer, working on frontend development using HTML, CSS, JavaScript, React.js, and Tailwind CSS, and backend development using Java and Spring Boot. Built REST APIs, integrated MySQL database using JPA/Hibernate, and collaborated with the team to deliver responsive and high-performance applications with improved user experience.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "Java",
+        "SpringBoot",
+        "MySQL",
+        "RestApi",
+        "Hibernate",
+        "Git",
+      ],
+    },
+    {
+      id: 2,
+      img: Education4ol,
+      role: "Web Developer Intern",
+      company: "Education4ol",
+      date: "Feb 2023 - April 2023",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: smt,
+      school: "Nbn Sinhgad Technical Campus , Pune",
+      date: "July 2021 - July 2024",
+      grade: "8.06 CGPA",
+      desc: "I have completed my Bachelor's degree in Computer Science and Engineering from Savitribai Phule University, Pune. During my time at Nbn Sinhgad Technical Campus, Pune, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Nbn Sinhgad Technical Campus has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Computer Science and Engineering - B.E. (CSE)",
+    },
+    {
+      id: 1,
+      img: newEng,
+      school: "New English School ,Parner",
+      date: "Sept 2019 - Aug 2020",
+      grade: "63%",
+      desc: "I completed my class 12 education from New English School, Parner, under the Maharashtra board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "Maharashtra Board (XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: Engineering,
+      school: "Smt S.G Lanke Secondary School,Parner",
+      date: "Apr 2017 - March 2018",
+      grade: "82%",
+      desc: "I completed my class 10 education from Smt S.G Lanke Secondary School,Parner, under the Maharashtra board, where I studied Science with Computer.",
+      degree: "Maharashtra Board (X) - Science with Computer",
+    },
+    
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Multivendor E-commerce Web App",
+      description:
+        "A full-stack e-commerce platform that allows multiple vendors to manage their products and customers to shop seamlessly. Built using React.js for the frontend and Java SpringBoot for the backend, with MySQL for database management.",
+      image: multivendorEcom,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API","Java", "SpringBoot", "MySQL", "Git"],
+      github: "https://github.com/aniket520/Multivendor-Ecommerce",
+      // webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Blogging-Application",
+      description:
+        "A full-stack blogging platform where users can create, manage, and publish blog posts. Built using React.js for the frontend and springBoot with Java for the backend, with MySQL for database management.",
+      image:BloggingApp ,
+      tags: ["React JS", "SpringBoot", "MySQL", "Api", "HTML", "CSS", "JavaScript","Java"],
+      github: "https://github.com/aniket520/Blogging_Application",
+      // webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "WebBrowser",
+      description:
+        "A custom web browser application built using JavaFX, providing users with a simple and intuitive interface for browsing the internet. It includes features such as tabbed browsing, bookmarking, and a built-in search engine for enhanced user experience.",
+      image: WebBrowser,
+      tags: ["JavaFX","Java"],
+      github: "https://github.com/aniket520/JavaFXBrowser",
+      // webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    // {
+    //   id: 3,
+    //   title: "Email Validator NPM Package",
+    //   description:
+    //     "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+    //   image: npmLogo,
+    //   tags: ["React JS", "Node.js", "NPM", "Validation"],
+    //   github: "https://github.com/codingmastr/cmtk-email-validator",
+    //   webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Task Reminder Chrome Extension Tool",
+    //   description:
+    //     "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    //   image: taskremLogo,
+    //   tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    //   github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    //   webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Webverse Digital",
+    //   description:
+    //     "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    //   image: webverLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    //   github: "https://github.com/codingmastr/Webverse-Digital",
+    //   webapp: "https://webversedigital.com/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
